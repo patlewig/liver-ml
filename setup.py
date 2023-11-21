b3dcfeac05674f6cee73dd8c0a6f9fd652d57c40
patlewig <patlewig@hotmail.com>
2023-11-20 21:32:55 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository supporting manuscript entitled A Comparison of ML approaches for predicting hepatotoxicity potential using chemical structure and targeted transcriptomic data.',
+    author='patlewig',
+    license='MIT',
+)
